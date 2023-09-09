060ff80918df436affb2abcf6015ee2a6262dc2f
Michael Mattingly <mikeamatt1@hotmail.com>
2023-09-08 21:06:42 -0400

    added hero

---
+++ client/src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import "./Hero.css";
+
+import { Container } from "react-bootstrap";
+
+const Hero = () => {
+  return (
+    <Container className="jumbotron">
+      <h1>Welcome to Logo MarketPlace</h1>
+    </Container>
+  );
+};
+
+export default Hero;
