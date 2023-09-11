f6065034ad7860801f3c7f9d6b92adbc1f499ffb
Michael Mattingly <mikeamatt1@hotmail.com>
2023-09-10 21:54:48 -0400

    implemented infinite scroll

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -5,10 +5,9 @@ const categorySeeds = require("./categorySeeds");
 const storeSeeds = require("./storeSeeds");
 const userSeeds = require("./userSeeds");
 const productSeeds = require("./productSeeds");
-const { create } = require("../models/Product");
 
 //Change these values to change the amount of data seeded
-const productAmount = 10;
+const productAmount = 1;
 const storeAmount = 100;
 const categoryAmount = 10;
 const userAmount = 10;
