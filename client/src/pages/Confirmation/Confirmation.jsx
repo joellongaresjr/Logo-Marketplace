65ab83b60f0bcdf595f9763f4904cf5756d29b96
ShirvanyanKaren <kshirvanyan2000@gmail.com>
2023-09-11 02:14:55 -0700

    confirmation page

--- client/src/pages/Confirmation/Confirmation.jsx
+++ client/src/pages/Confirmation/Confirmation.jsx
@@ -5,75 +5,120 @@ import { loadStripe } from "@stripe/stripe-js";
 import { useLazyQuery } from "@apollo/client";
 import { useSelector, useDispatch } from "react-redux";
 import { idbPromise } from "../../utils/helpers";
+import ConfirmationItem  from "../../components/ConfirmationItem/ConfirmationItem";
+import { ADD_MULTIPLE_TO_CART, REMOVE_FROM_CART } from "../../utils/actions";
+import Auth from "../../utils/auth";
 
 const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
 
 const Confirmation = () => {
-    const state = useSelector((state) => {
-        return state;
-    });
+
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+    const [checkoutCompleted, setCheckoutCompleted] = useState(false);
+    const state = useSelector((state) => state);
     const dispatch = useDispatch();
-    const [checkoutCompleted, setCheckoutCompleted] = useState(false); // Track checkout completion
-    
-    const [getCheckout, { data, loading }] = useLazyQuery(QUERY_CHECKOUT);
 
-    useEffect(() => {
-        async function saveOrder() {
-            const order = data.checkout.session;
-            console.log(order);
-            dispatch({ type: "ADD_MULTIPLE_TO_CART", products: [] });
-            order.products.forEach((item) => {
-                idbPromise("cart", "delete", item);
-            });
-            setCheckoutCompleted(true); // Mark checkout as completed
+useEffect(() => {
+    async function getCart() {
+        const cart = await idbPromise("cart", "get");
+        dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+    }
+    if (!state.cart.length) {
+        getCart();
+    }
+}, [state.cart.length, dispatch]);
+
+function calculateTotal() {
+    let sum = 0;
+    state.cart.forEach((item) => {
+        sum += item.price * item.purchaseQuantity;
+    });
+
+    return sum.toFixed(2);
+}
+
+function totalItems() {
+    let total = 0;
+    state.cart.forEach((item) => {
+        total += item.purchaseQuantity;
+    });
+
+    return total;
+}
+
+function submitCheckout() {
+    const productIds = [];
+
+    state.cart.forEach((item) => {
+        for (let i = 0; i < item.purchaseQuantity; i++) {
+            productIds.push(item._id);
         }
+    });
+
+    getCheckout({
+        variables: { products: productIds },
+    });
+}
+    onChange = (e) => {
+        const value = e.target.value;
+        if (value === "0") {
+            dispatch({
+                type: REMOVE_FROM_CART,
+                _id: item._id,
+            });
+            idbPromise("cart", "delete", { ...item });
+        } else {
+            dispatch({
+                type: UPDATE_CART_QUANTITY,
+                _id: item._id,
+                purchaseQuantity: parseInt(value),
+            });
+            idbPromise("cart", "put", { ...item, purchaseQuantity: parseInt(value) });
+        }
+    };
 
 
-    }, [data, loading, getCheckout, dispatch]);
 
-    function submitCheckout() {
-        const productIds = [];
 
-        state.cart.forEach((item) => {
-            for (let i = 0; i < item.purchaseQuantity; i++) {
-                productIds.push(item._id);
-            }
-        });
-    }
-
-    function totalItems(cart) {
-        return cart.reduce((sum, i) => sum + i.quantity, 0);
-    }
-
-    function calculateTotal(cart) {
-        return cart.reduce((sum, i) => sum + i.price * i.quantity, 0).toFixed(2);
-    }
 
     return (
-        <div className="container">
-            <h2>Checkout</h2>
-            <h3>
-                {state.cart.length
-                    ? `Order ${totalItems(state.cart)} item(s) for ${calculateTotal(
-                          state.cart
-                      )}`
-                    : "Your cart is empty"}
-            </h3>
-            {state.cart.length ? (
-                <button onClick={submitCheckout}>Checkout</button>
-            ) : null}
-            {checkoutCompleted && (
-                <div>
-                    <h2>Thank you for your purchase!</h2>
-                    <h3>
-                        You will now be redirected to the payment page. If you are not
-                        redirected, please click the button below.
-                    </h3>
-                    <button onClick={() => getCheckout()}>Checkout</button>
+        <div className="confirmation">
+            <h2> Confirmation</h2>
+            <div className="confirmation-container">
+                <div className="confirmation-items">
+                    {state.cart.length ? (
+                        <>
+                            {state.cart.map((item) => (
+                                <ConfirmationItem key={item._id} item={item} />
+                            ))}
+                            {Auth.loggedIn() ? (
+                                <button onClick={submitCheckout}>
+                                    <a href="/confirmation">Checkout</a>
+                                </button>
+                            ) : (
+                                <span>(log in to check out)</span>
+                            )}
+                        </>
+                    ) : (
+                        <h3>Nothing in your cart yet!</h3>
+                    )}
                 </div>
-            )}
+                <div className="confirmation-total">
+                    <h3>
+                        Total: ${calculateTotal()}
+                    </h3>
+                    <h3>
+                        Total Items: {totalItems()}
+                    </h3>
+                </div>
+            </div>
         </div>
-    );
-};
+
+                        
+    )
+}
+       
+            
+
 
 export default Confirmation;
