3529ef186456141ecf34f9e1bad30340ac5513af
Michael Mattingly <mikeamatt1@hotmail.com>
2023-09-13 23:44:47 -0400

    committing successpage

--- client/src/pages/Success/Success.jsx
+++ client/src/pages/Success/Success.jsx
@@ -8,19 +8,20 @@ import { useQuery } from "@apollo/client";
 import { QUERY_USER } from "../../utils/queries";
 
 import "./SuccessStyles.css";
-import { useQuery } from '@apollo/client';
 
 const Success = () => {
   const token = localStorage.getItem("id_token");
   const decodedToken = Auth.getProfile(token);
   const globalCart = useSelector((state) => state.cart);
-  const dispatch = useDispatch();
   const storedCart = idbPromise("cart", "get");
+  const [formComplete, setFormComplete] = useState(false);
   const [formState, setFormState] = useState({
     user_name: "",
     user_email: "",
     message: "",
   });
+
+  console.log(decodedToken);
 
   const [cart, setCart] = useState([]);
   let products = [];
@@ -31,86 +32,94 @@ const Success = () => {
         if (item.name) {
           products.push(item);
         }
-      })
+      });
       setCart(products);
       products.map((item) => {
         idbPromise("cart", "delete", item);
-       });
+      });
+      const stringEmail = JSON.stringify(decodedToken.data.email);
+      const stringName = JSON.stringify(decodedToken.data.username);
+
+      console.log(stringEmail);
+      console.log(stringName);
+
+
+      setFormState({
+        ...formState,
+        user_email: stringEmail,
+        user_name: stringName
+      });
     }
     getCart();
   }, []);
+
+
 
   console.log("this is the stored cart", storedCart);
   console.log("this is the global", globalCart);
   console.log("this is the local", cart);
 
-  const sendInvoice = () => {
+  const createMessage = () => {
     const cartItems = cart;
     const cartInfo = [];
     cartItems.forEach((item, index) => {
-      cartInfo.push(`${index + 1}. ${item.name} - Price: $${
-        item.price
-      }, Quantity: ${item.purchaseQuantity}\n`);
+      cartInfo.push(
+        `${index + 1}. ${item.name} - Price: $${item.price}, Quantity: ${item.purchaseQuantity
+        }\n`
+      );
     });
-    const userInfo = idbPromise("shipping", "get");
     const message = `
     Thank you for your purchase!
 
     ${cartInfo}
 
-    If you have any questions or concerns, please feel free to contact us.`
+    If you have any questions or concerns, please feel free to contact us.`;
 
-    emailjs.
-
-
-
+    setFormState({ ...formState, message: message });
+    return message;
    };
 
 
+  const sendInvoice = () => {
+    const cartItems = cart;
+    const cartInfo = [];
+    cartItems.forEach((item, index) => {
+      cartInfo.push(
+        `${index + 1}. ${item.name} - Price: $${item.price}, Quantity: ${item.purchaseQuantity
+        }\n`
+      );
+    });
+    const message = `
+    Thank you for your purchase!
 
-  // const message = `
-  // Thank you for your purchase`;
+    ${cartInfo}
 
-  // const sendEmail = (e) => {
-  //   e.preventDefault();
+    If you have any questions or concerns, please feel free to contact us.`;
 
-  //   cartItems.forEach((item, index) => {
-  //     cartInfo += `${index + 1}. ${item.productName} - Price: $${
-  //       item.price
-  //     }, Quantity: ${item.quantity}\n`;
-  //   });
-
-  //   // Complete the email message
-  //   const message = `
-  //     Thank you for your purchase!
-
-  //     ${cartInfo}
-
-  //     If you have any questions or concerns, please feel free to contact us.
-  // `;
-
-  //   emailjs
-  //     .sendForm(
-  //       "service_9v3d86w",
-  //       "template_d90459p",
-  //       form.current,
-  //       "7Fjxh4cd-qP264EKp"
-  //     )
-  //     .then(
-  //       (result) => {
-  //         console.log(result.text);
-  //         setFormState({
-  //           user_name: "",
-  //           user_email: "",
-  //           message: "",
-  //         });
-  //         setFormSubmitted(true);
-  //       },
-  //       (error) => {
-  //         console.log(error.text);
-  //       }
-  //     );
-  // };
+    setFormState({ ...formState, message: message });
+    console.log(formState)
+    emailjs
+      .sendForm(
+        "service_9v3d86w",
+        "template_d90459p",
+        formState.current,
+        "7Fjxh4cd-qP264EKp"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setFormState({
+            user_name: "",
+            user_email: "",
+            message: "",
+          });
+          setFormSubmitted(true);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
   return (
     <div className="success-page">
