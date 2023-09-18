4db97f13f6dd54f77aa657ef8218a5ca6b5635b0
ShirvanyanKaren <kshirvanyan2000@gmail.com>
2023-09-17 21:19:55 -0700

    fixed up styled for success on mobile

--- client/src/pages/Success/Success.jsx
+++ client/src/pages/Success/Success.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { idbPromise } from "../../utils/helpers";
